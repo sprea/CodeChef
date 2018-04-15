8509dda4c19faa71996ae74046b182722552a2de
sprea <spriha27@gmail.com>
2018-04-16 00:19:56 +0530

    Chef and Remissness

---
+++ REMISS.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+	int t,n,m;
+
+	cin>>t;
+
+	for(int i=0; i<t; i++){
+
+	 cin>>n>>m;
+
+	 if(n>m)
+
+	 cout<<n<<" "<<n+m<<endl;
+
+	 else
+
+	 cout<<m<<" "<<n+m<<endl;
+
+	}
+
+	return 0;
+
+} 
+
